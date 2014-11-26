44a07bebe90f73a4d1db1ae405d5617b5553f8ae
Tim Abell <tim@timwise.co.uk>
2014-11-26 15:31:51 +0000

    Updated .bat comments - new url

--- ScriptDBDiagrams.bat
+++ ScriptDBDiagrams.bat
@@ -1,9 +1,10 @@
 @echo off
-REM Reads diagrams from databases listed below and writes them out as sql files
-REM Source: http://joelmansford.wordpress.com/2008/04/01/scripting-sql-server-diagrams-to-files-for-source-control/
 
-REM  Powershell scripts must be enabled by running `Set-ExecutionPolicy RemoteSigned` as administrator in powershell. Answer Y at the prompt.
-REM  You have to manually install the two .sql files in lib in each of the databases to be read for this to work.
+REM Usage:
+REM * Powershell scripts must be enabled by running `Set-ExecutionPolicy RemoteSigned` as administrator in powershell. Answer Y at the prompt.
+
+REM About:
+REM * Reads diagrams from databases listed below and writes them out as sql files
+REM * https://github.com/timabell/database-diagram-scm
 
 powershell lib\ScriptDBDiagrams.ps1 -server '(LocalDb)\v11.0' -database YourDatabase -outputFolder "diagrams"
-
