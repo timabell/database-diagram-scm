201c04639e594e54b30d24c669835d83467712a3
Tim Abell <tim@timwise.co.uk>
2015-06-24 10:51:56 +0100

    support environment variables when exporting

--- ScriptDBDiagrams.bat
+++ ScriptDBDiagrams.bat
@@ -1,4 +1,12 @@
+REM depends on sqlcmd which comes as part of ssms
+REM connects to default instance on localhost by default
+
 @echo off
+Setlocal EnableDelayedExpansion
+Setlocal EnableExtensions
+
+if "!server!"==""   set server=.
+if "!database!"=="" set database=ExampleDb
 
 REM Usage:
 REM * Powershell scripts must be enabled by running `Set-ExecutionPolicy RemoteSigned` as administrator in powershell. Answer Y at the prompt.
@@ -7,4 +15,4 @@ REM About:
 REM * Reads diagrams from databases listed below and writes them out as sql files
 REM * https://github.com/timabell/database-diagram-scm
 
-powershell lib\ScriptDBDiagrams.ps1 -server '(LocalDb)\v11.0' -database YourDatabase -outputFolder "diagrams"
+powershell lib\ScriptDBDiagrams.ps1 -server %server% -database %database% -outputFolder "diagrams"
