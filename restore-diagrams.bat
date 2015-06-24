546024a8287b92dbb842138f8505af681418d168
Tim Abell <tim@timwise.co.uk>
2015-06-24 10:43:00 +0100

    batch file for restoring diagrams
    fixes #2

---
+++ restore-diagrams.bat
@@ -0,0 +1,11 @@
+REM depends on sqlcmd which comes as part of ssms
+REM connects to default instance on localhost by default
+
+@echo off
+Setlocal EnableDelayedExpansion
+Setlocal EnableExtensions
+
+if "!server!"==""   set server=.
+if "!database!"=="" set database=ExampleDb
+
+for /f %%a IN ('dir /b /s *.sql') do call sqlcmd -S %server% -d %database%  -i %%a
